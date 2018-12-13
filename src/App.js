ca25b97eb4c63c79ca38464c0a7bc2d4d04a4329
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-12-13 13:35:32 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React from 'react';
+// import './App.css';
+
+
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    )
+  }
+}
+
+export default App;
